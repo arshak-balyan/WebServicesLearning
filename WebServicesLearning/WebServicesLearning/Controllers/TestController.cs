b18c54d0ee59d02df9892a83883d41c1bb34a2a3
Arshak Balyan <arshak.balyan@yahoo.com>
2025-03-23 01:22:25 +0400

    get tasks, 1 and 2

--- WebServicesLearning/WebServicesLearning/Controllers/TestController.cs
+++ WebServicesLearning/WebServicesLearning/Controllers/TestController.cs
@@ -1,6 +1,55 @@
-﻿namespace WebServicesLearning.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace WebServicesLearning.Controllers
 {
-    public class TestController
+    public class Data
     {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly List<Data> records = new()
+        {
+            new Data { ID = 1, UserID = 1, Title = "qui est esse", Content = "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut" },
+            new Data { ID = 2, UserID = 2, Title = "bla bla bla", Content = "a lot of text" },
+            new Data { ID = 3, UserID = 3, Title = "ea molestias quasi exercitationem repellat qui ipsa sit aut", Content = "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut" }
+        };
+
+        [HttpGet]
+        public IActionResult GetByIDandTitle(int userID, string title)
+        {
+            var searchInData = records
+                .Where(p => p.UserID == userID && p.Title == title)
+                .ToList();
+
+            if (!searchInData.Any())
+            {
+                return NotFound("No data found.");
+            }
+
+            return Ok(searchInData);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetByID(int id)
+        {
+            var searchInData = records
+                .Where(p => p.UserID == id)
+                .ToList();
+
+            if (!searchInData.Any())
+            {
+                return NotFound("No data found.");
+            }
+
+            return Ok(searchInData);
+        }
     }
 }
